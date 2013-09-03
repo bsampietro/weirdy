7b112f9b0e86cfd74b9b5aadd16f1c57926371bd
Bruno Sampietro <bsampietro@gmail.com>
2013-09-03 14:54:24 -0300

    updated README.rd and version

--- test/dummy/app/controllers/home_controller.rb
+++ test/dummy/app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
   def index
-    shims
     #User.first.admin?
-  end
-  
-  def shims
-    raise "not compiling"
+    methods = (1..10).to_a.map {|i| "met#{i}"}
+    methods.each do |met|
+      #self.class.send(:define_method, met) {raise "#{met} is wrong!"}
+      User.send(:define_method, met) {raise "#{met} is wrong!"}
+    end
+    1.upto(10) do |i|
+      #self.send(methods.sample)
+      User.new.send(methods.sample)
+    end
   end
 end
